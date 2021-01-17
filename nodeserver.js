4b07ef0aef54745bf8ab76014340b83bd4559f96
CNavo <chrisnavo01@gmail.com>
2021-01-16 16:08:42 -0800

    Updated http server assignment

--- nodeserver.js
+++ nodeserver.js
@@ -2,17 +2,29 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer(function (request, response) {
-    if (request.url == '/users') {
+    if (request.method == 'GET' && request.url == '/users') {
+        let requestGet = request.method;
+        console.log(requestGet);
+
         fs.readFile('names.html', function(error, file) {
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(file);
             return response.end();
         });
     }    
-    else if (request.url == '/json') {
-        fs.readFile('names.json', function(error, file) {
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(file);
+    else if (request.method == 'POST') {
+        response.writeHead(200, {
+          'Content-Type': 'application/json'});
+
+        let post = request.method;
+        console.log(post);
+        fs.readFile('names.json', function (error, data) {
+            if (error) {
+                response.writeHead(404);
+                response.write('Invalid Request!');
+            } else {
+               response.write(data);
+            }
             return response.end();
         });
     }
